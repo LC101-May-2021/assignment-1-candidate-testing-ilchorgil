b622c5835dc9c4a57fe2ed1563ad9300d40f3514
Ilch Orgil <>
2021-05-24 21:42:52 +0000

    deleted empty space between the quotation marks

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,13 +3,13 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = " ";
+let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
 let question = ("Who was the first American woman in space? ");
 let correctAnswer = ("Sally Ride");
-let candidateAnswer = (" ");
+let candidateAnswer = ("");
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -18,7 +18,7 @@ let candidateAnswers;
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   
-  candidateName = input.question("What's your name?\n")
+  candidateName = input.question("What's your name?\n");
 }
 
 function askQuestion() {
