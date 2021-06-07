379a499fc73be71afbe0d82cfb72bb6eb7fc25df
Ilch Orgil <>
2021-06-07 01:53:25 +0000

    Finished graded assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,11 +15,11 @@ let points = 0;
 let totals = "";
 
 let questions = [
-  "Who was the first American woman in space? ",
-  "True or false: 5 kilometer == 5000 meters? ",
-  "(5 + 3)/2 * 10 = ? ", 
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
-  "What is the minimum crew size for the ISS? "];
+  "1) Who was the first American woman in space? ",
+  "2) True or false: 5 kilometer == 5000 meters? ",
+  "3) (5 + 3)/2 * 10 = ? ", 
+  "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "5) What is the minimum crew size for the ISS? "];
 let correctAnswers = [
   "Sally Ride", 
   "true", 
@@ -29,7 +29,7 @@ let correctAnswers = [
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   
-  candidateName = input.question("What's your name? ");
+  candidateName = input.question("Candidate Name: ");
 }
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
